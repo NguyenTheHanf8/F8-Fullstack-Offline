afa727817d33514b4492c96894a2f9b205cb8df7
NguyenTheHanf8 <nguyenthehan03@gmail.com>
2024-09-07 14:44:16 +0700

    change

--- Day-19/Js/ex04.js
+++ Day-19/Js/ex04.js
@@ -2,7 +2,7 @@ function isPrime(n) {
   // Kiểm tra các trường hợp đặc biệt
   if (n <= 1) return false;
   if (n === 2) return true;
-  // Kiểm tra các số chia hết từ 2 đến căn bậc hai của n
+  // Kiểm tra các số chia hết từ 2 đến n
   for (let i = 2; i <= n; i++) {
     if (n % i === 0) return false;
   }
@@ -13,6 +13,8 @@ function isPrime(n) {
 const number = prompt("Nhập một số bất kỳ"); // Thay đổi số này để kiểm tra
 if (isPrime(number)) {
   console.log(`${number} là số nguyên tố.`);
+  alert(`${number} là số nguyên tố.`);
 } else {
   console.log(`${number} không phải là số nguyên tố.`);
+  alert(`${number} không phải là số nguyên tố.`);
 }
