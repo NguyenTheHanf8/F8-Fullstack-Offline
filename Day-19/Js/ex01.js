1ea62f446ea1c2af293abdb83b25019111f02e50
NguyenTheHanf8 <nguyenthehan03@gmail.com>
2024-09-07 14:59:52 +0700

    change

--- Day-19/Js/ex01.js
+++ Day-19/Js/ex01.js
@@ -2,7 +2,9 @@ function caclTaxiPostage(km) {
   let postage = 0;
 
   // Tính giá cước của taxi theo từng mốc quãng đường
-  if (km > 0 && km <= 1) {
+  if (km <= 0) {
+    postage = 0;
+  } else if (km > 0 && km <= 1) {
     postage = km * 15000;
   } else if (km > 1 && km <= 5) {
     postage = 15000 + (km - 1) * 13500;
@@ -18,6 +20,15 @@ function caclTaxiPostage(km) {
   return postage;
 }
 
+// Nhập quảng đường định đi
+const km = prompt(`Quãng đường bạn định đi(km): `);
+const totalBill = caclTaxiPostage(km);
+
+// Hiển thị kết quả
+alert(`Số tiền phải trả cho ${km}km là: ${totalBill} VND`);
+console.log(`Số tiền phải trả cho ${km}km là: ${totalBill} VND`);
+
+// Test value
 const testValues = [0.5, 1, 3, 5, 6, 120, 130];
 testValues.forEach((km) => {
   console.log(`Số km: ${km}, Tiền cước: ${caclTaxiPostage(km)}đ`);
