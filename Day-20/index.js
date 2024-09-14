c6489c4c902134a13f95b4449faa14a8f23df713
NguyenTheHanf8 <nguyenthehan03@gmail.com>
2024-09-14 17:52:57 +0700

    Bài tập ngày 20

---
+++ Day-20/index.js
@@ -0,0 +1,23 @@
+// Màu chạy qua các từ
+const changeColor = "red";
+const defaultColor = "black";
+
+// Chọn đoạn văn bản cần tạo hiệu ứng
+const text = document.getElementById("animation-text");
+
+// Phân chia đoạn văn thành từng từ và bọc nó trong thẻ span
+const words = text.innerText.split(" ");
+text.innerHTML = words.map((word) => `<span>${word}</span>`).join(" ");
+
+const spans = text.querySelectorAll("span"); // Lấy danh sách các span
+let currentIndex = 0;
+// Hàm thay đổi màu sắc chạy qua các từ
+function animateColor() {
+  // Đặt lại màu của tất cả các từ về màu mặc định
+  spans.forEach((span) => {
+    span.style.color = defaultColor;
+  });
+  spans[currentIndex].style.color = changeColor; // Tô màu chính cho từ hiện tại
+  currentIndex = (currentIndex + 1) % spans.length; // Chuyển sang từ tiếp theo
+}
+setInterval(animateColor, 1000); // Set thời gian chạy
